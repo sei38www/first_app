6ce192301af574bfe3262990b823ba5a78608a5b
sei38www <worlds38end@gmail.com>
2021-11-02 12:16:13 +0900

    first commit

---
+++ db/migrate/20210911125608_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+
+      t.timestamps
+    end
+  end
+end
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.timestamps
+    end
+  end
+end
